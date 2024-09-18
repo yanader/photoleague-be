eae834d53d60f97d0a08d93077786d9fe0795494
ste <mrbarrow@hotmail.co.uk>
2024-09-18 19:29:47 +0100

    feat: User

---
+++ src/main/java/com/photoleague/entity/User.java
@@ -0,0 +1,16 @@
+package com.photoleague.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "user_table")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
--- src/main/java/com/photoleague/service/impl/CommentServiceImpl.java
+++ src/main/java/com/photoleague/service/impl/CommentServiceImpl.java
@@ -1,11 +1,9 @@
 package com.photoleague.service.impl;
 
-import com.photoleague.mapper.CommentMapper;
 import com.photoleague.entity.Comment;
 import com.photoleague.entity.dto.CommentDTO;
 import com.photoleague.repository.CommentRepository;
 import com.photoleague.service.CommentService;
-import com.photoleague.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +14,6 @@ import java.util.List;
 public class CommentServiceImpl implements CommentService {
 
     private final CommentRepository commentRepository;
-    private final CommentMapper commentMapper;
 
     @Override
     public void save(CommentDTO commentDTO) {
