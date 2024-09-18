b10c5a8018b8a332d7a7bda2864d05c054a0d653
ste <mrbarrow@hotmail.co.uk>
2024-09-18 20:42:34 +0100

    feat: UserController

--- src/main/java/com/photoleague/controller/HealthController.java
+++ src/main/java/com/photoleague/controller/HealthController.java
@@ -1,13 +1,11 @@
 package com.photoleague.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
-@CrossOrigin
 public class HealthController {
 
     @GetMapping
---
+++ src/main/java/com/photoleague/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.photoleague.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principle) {
+        return principle.getAttributes();
+    }
+}
