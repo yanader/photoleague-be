3645443910422d9ace3426a003523d16e6dd5459
ste <mrbarrow@hotmail.co.uk>
2024-09-27 20:41:51 +0100

    feat:UserController

--- src/main/java/com/photoleague/controller/UserController.java
+++ src/main/java/com/photoleague/controller/UserController.java
@@ -1,52 +1,38 @@
 package com.photoleague.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.web.bind.annotation.*;
 
-import javax.sql.DataSource;
+import com.photoleague.model.User;
+import com.photoleague.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 import java.util.Map;
 
-
 @RestController
-@RequestMapping("/api")
 public class UserController {
 
     @Autowired
-    DataSource datasource;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @PostMapping("/public/users")
-    public String createUser(@RequestParam String username,
-                             @RequestParam String password,
-                             @RequestParam String role) {
-        JdbcUserDetailsManager userDetailsManager
-                = new JdbcUserDetailsManager(datasource);
-
-        if (userDetailsManager.userExists(username)) {
-            return "User already exists!";
-        }
-
-        UserDetails user = User.withUsername(username)
-                .password(passwordEncoder.encode(password))
-                .roles(role)
-                .build();
-
-        userDetailsManager.createUser(user);
-
-        return "User created successfully";
-    }
+    private UserService userService;
 
     @GetMapping("/user-info")
-    public Map<String, Object> getUser(
+    public Map<String, Object> user(
             @AuthenticationPrincipal OAuth2User principal) {
-        return principal.getAttributes();
+
+
+        User user = userService.processOAuthPostLogin(principal);
+
+        Map<String, Object> userInfo = new HashMap<>();
+        userInfo.put("name", user.getName());
+        userInfo.put("email", user.getEmail());
+        userInfo.put("picture", user.getImgUrl());
+
+        return userInfo;
     }
 }
