d756ea7fcaf10f2523b5b759df7dd5b913e1ecd9
ste <mrbarrow@hotmail.co.uk>
2024-09-27 20:35:32 +0100

    feat:User

---
+++ src/main/java/com/photoleague/model/User.java
@@ -0,0 +1,61 @@
+package com.photoleague.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String imgUrl;
+
+    @Column(nullable = false)
+    private String loginSource;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getLoginSource() {
+        return loginSource;
+    }
+
+    public void setLoginSource(String loginSource) {
+        this.loginSource = loginSource;
+    }
+
+}
