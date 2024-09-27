aa85de218e9aab78a5f6c32e02ecb117d8b4442e
ste <mrbarrow@hotmail.co.uk>
2024-09-27 20:47:03 +0100

    refactor:cleanup unused imports

--- src/main/java/com/photoleague/config/SecurityConfig.java
+++ src/main/java/com/photoleague/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.photoleague.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,7 +17,6 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
-                //.formLogin(form -> form.defaultSuccessUrl("/hello", true));
                 .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:3000/dashboard", true));
         return http.build();
     }
--- src/main/java/com/photoleague/controller/UserController.java
+++ src/main/java/com/photoleague/controller/UserController.java
@@ -1,13 +1,9 @@
 package com.photoleague.controller;
 
-
 import com.photoleague.model.User;
 import com.photoleague.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
